bab184aeb9bb5c0430b2c863327e6f784951db0a
marcelopetroni <marceloapetroni@hotmail.com>
2024-08-27 16:33:22 -0300

    possíveis componentes e rotas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { FeedComponent } from './feed/feed.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'feed', component: FeedComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: '/feed', pathMatch: 'full' },  
+];
