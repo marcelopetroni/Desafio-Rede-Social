2de8e01e7009ed4421f1e1c01d59466c4b258bb9
marcelopetroni <marceloapetroni@hotmail.com>
2024-08-29 11:01:32 -0300

    new post route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { FeedComponent } from './feed/feed.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AddPostComponent } from './add-post/add-post.component';
 
 export const routes: Routes = [
   { path: 'feed', component: FeedComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'newPost', component: AddPostComponent },
   { path: '', redirectTo: '/feed', pathMatch: 'full' },  
+  
 ];
