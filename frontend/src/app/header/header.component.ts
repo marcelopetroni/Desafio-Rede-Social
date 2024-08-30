de94293b306f89ee9af1c838e5bae664587fa5f8
marcelopetroni <marceloapetroni@hotmail.com>
2024-08-29 21:32:34 -0300

    componente new post

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule, Event, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.sass'
+  styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent {
+  isAddPostRoute: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.isAddPostRoute = this.router.url === '/newPost';
+      }
+    });
+  }
+
+  navigateToFeed() {
+    this.router.navigate(['/feed']);
+  }
 }
